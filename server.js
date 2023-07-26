57e96c4de0af8d6ab0d4e3287bbbffe1d4d9451d
Ethan Daniel <e.daniel.small@gmail.com>
2023-07-26 12:03:07 -0700

    init commit - finished project

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const apiRoutes = require('./Develop/routes/apiRoutes');
+const htmlRoutes = require('./Develop/routes/htmlRoutes');
+
+// Creating port and starting server
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'))
+//Use routes information
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes)
+
+//Listening port
+app.listen(PORT, () =>
+    console.log(`Server available at http://localhost:${PORT}`)
+);

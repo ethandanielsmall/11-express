154d33e4c527d5b035f5e31573c5e0dd7c73127f
Ethan Daniel <e.daniel.small@gmail.com>
2023-08-02 18:09:08 -0700

    final sever functional !

--- server.js
+++ server.js
@@ -9,19 +9,26 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/', html);
 
+// Specific routes
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, 'Develop/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'Develop/public/notes.html'))
+);
+
+// Static files middleware
 app.use(express.static('public'));
 
-// GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+// Catch-all route for HTML routes
+app.use('/', html);
 
-// GET Route for feedback page
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
+// Catch-all route for handling 404 errors
+app.use((req, res) => {
+  res.status(404).send('Page not found');
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)

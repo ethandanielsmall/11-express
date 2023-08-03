0b85903a02f1ccebf671e23147c29968a4a6c4f0
Ethan Daniel <e.daniel.small@gmail.com>
2023-08-02 17:05:01 -0700

    trying to make the routes functional

--- server.js
+++ server.js
@@ -1,26 +1,3 @@
-// const express = require('express');
-// const apiRoutes = require('./Develop/routes/apiRoutes');
-// const htmlRoutes = require('./Develop/routes/htmlRoutes');
-
-// // Creating port and starting server
-// const PORT = process.env.PORT || 3001;
-// const app = express();
-
-// app.use(express.json());
-// // app.use(express.urlencoded({ extended: true }));
-// app.use(express.static('public'))
-
-// //Use routes information
-// app.use('/api', apiRoutes);
-// app.use('/', htmlRoutes);
-
-// //Listening port
-// app.listen(PORT, () =>
-//     console.log(`Server available at http://localhost:${PORT}`)
-// );
-
-// --------------------------------------------------------
-
 const express = require('express');
 const path = require('path');
 const html = require('./Develop/routes/htmlRoutes.js');
@@ -42,8 +19,8 @@ app.get('/', (req, res) =>
 );
 
 // GET Route for feedback page
-app.get('/feedback', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 app.listen(PORT, () =>
